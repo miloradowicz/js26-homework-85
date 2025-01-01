f7c6e62646d5815ade05ec3468150e6379f3ee8d
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-01-02 04:08:21 +0500

    server set up

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import config from './config';
+import artists from './routers/artists';
+import albums from './routers/albums';
+import tracks from './routers/tracks';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/artists', artists);
+app.use('/albums', albums);
+app.use('/tracks', tracks);
+
+(async () => {
+  await mongoose.connect(new URL(config.mongo.db, config.mongo.host).href);
+
+  app.listen(config.express.port, () => {
+    console.log(`Server ready on port http://localhost:${config.express.port}`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+})().catch(console.error);
---
+++ routers/artists.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+export default router;
