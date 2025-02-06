08a3d899ef2083e60ed61d0d7003bd5cc5f0d958
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-02-06 12:45:41 +0600

    [hw-91] model & fixtures updated

--- api/models/User.ts
+++ api/models/User.ts
@@ -1,13 +1,16 @@
-import mongoose, { HydratedDocument } from 'mongoose';
-import bcrypt from 'bcrypt';
-import { randomUUID } from 'crypto';
+import mongoose, { HydratedDocument } from "mongoose";
+import bcrypt from "bcrypt";
+import { randomUUID } from "crypto";
 
-import config from '../config';
+import config from "../config";
 
 export interface Fields {
   _id: mongoose.Types.ObjectId;
   username: string;
   password: string;
+  displayName: string;
+  googleId: string | null;
+  avatarUrl: string | null;
   role: string;
   token: string | null;
 }
@@ -24,24 +27,36 @@ const schema = new mongoose.Schema<HydratedDocument<Fields>, Model, Methods>(
   {
     username: {
       type: String,
-      required: [true, 'Username is required'],
+      required: [true, "Username is required"],
       unique: true,
       validate: {
         validator: async function (this: HydratedDocument<Fields>, value: string): Promise<boolean> {
-          return !this.isModified('username') || !(await User.findOne({ username: value }));
+          return !this.isModified("username") || !(await User.findOne({ username: value }));
         },
-        message: 'Username already occupied',
+        message: "Username already occupied",
       },
     },
     password: {
       type: String,
-      required: [true, 'Password is required'],
+      required: [true, "Password is required"],
+    },
+    displayName: {
+      type: String,
+      required: [true, "Display name is required"],
+    },
+    googleId: {
+      type: String,
+      default: null,
+    },
+    avatarUrl: {
+      type: String,
+      default: null,
     },
     role: {
       type: String,
-      enum: ['user', 'admin'],
-      required: [true, 'Role is required'],
-      default: 'user',
+      enum: ["user", "admin"],
+      required: [true, "Role is required"],
+      default: "user",
     },
     token: {
       type: String,
@@ -49,13 +64,13 @@ const schema = new mongoose.Schema<HydratedDocument<Fields>, Model, Methods>(
     },
   },
   {
-    strict: 'throw',
+    strict: "throw",
     versionKey: false,
-  }
+  },
 );
 
-schema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+schema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(config.saltWorkFactor);
   const hash = await bcrypt.hash(this.password, salt);
@@ -63,7 +78,7 @@ schema.pre('save', async function (next) {
   this.password = hash;
 });
 
-schema.set('toJSON', {
+schema.set("toJSON", {
   transform: (_doc, ret) => {
     delete ret.password;
     return ret;
@@ -82,6 +97,6 @@ schema.methods.generateToken = function () {
   this.token = randomUUID();
 };
 
-const User = mongoose.model<HydratedDocument<Fields>, Model>('User', schema);
+const User = mongoose.model<HydratedDocument<Fields>, Model>("User", schema);
 
 export default User;
